cec9aa328d60b203de5675a77995df6f653db559
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-21 21:38:12 -0400

    Update views for CartItem

--- aeca/shop/urls.py
+++ aeca/shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("cart/", views.cart, name="cart"),
     path("orders/", views.orders, name="orders"),
+    path("add/", views.add_product_to_cart, name="add"),
 ]
--- aeca/shop/views.py
+++ aeca/shop/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Product
+from .models import CartItem, Product
 
 
 def index(request):
-    context = {"products": Product.objects.all()}
+    context = {
+        "products": Product.objects.all(),
+        "cart": CartItem.objects.filter(user=request.user.id)
+    }
     return render(request, "shop/index.html", context)
 
 
+def add_product_to_cart(request):
+    product_id = request.POST["product_id"]
+    product = Product.objects.filter(id=product_id).get()
+    item = CartItem(user=request.user, product=product)
+    item.save()
+    return redirect("index")
+
+
 def cart(request):
-    return render(request, "shop/cart.html", {})
+    context = {
+        "cart": CartItem.objects.filter(user=request.user.id)
+    }
+    return render(request, "shop/cart.html", context)
 
 
 def orders(request):
