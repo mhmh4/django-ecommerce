f6062d073078f79a9589420a3162474e94506b63
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-22 14:36:57 -0400

    Update admin.py

--- aeca/shop/admin.py
+++ aeca/shop/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import CartItem, Order, OrderItem, Product
 
+admin.site.register(CartItem)
+admin.site.register(Order)
+admin.site.register(OrderItem)
 admin.site.register(Product)
