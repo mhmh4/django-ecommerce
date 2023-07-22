9c60374c4c366abe81cff7f40fe97704c8af7041
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-07-21 21:37:47 -0400

    Create `CartItem` model

--- aeca/shop/models.py
+++ aeca/shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
@@ -9,3 +10,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user}: {self.product}"
