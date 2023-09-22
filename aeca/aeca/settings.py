86894348803e628311f70ba6b0d5c68edae41218
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-09-21 20:10:50 -0400

    Switch back to SQLite

--- aeca/aeca/settings.py
+++ aeca/aeca/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = "aeca.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "aeca",
-        "USER": "postgres",
-        "PASSWORD": "password",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
